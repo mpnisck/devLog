ce1357047ef3b7b4266b958230dcb3905a870998
YeonJu <mpnisck@gmail.com>
2025-04-25 16:48:33 +0900

    chore: Tailwind CSS 초기 세팅

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-[#f00]">Hello, world!</h1>
+      <p>This is a simple React app.</p>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import "@/styles/style.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
