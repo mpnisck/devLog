d07f3fe94b51326cb202a42360f93a905793b781
YeonJu <mpnisck@gmail.com>
2025-04-28 15:54:19 +0900

    feat: Career 화면 기본 구조

---
+++ src/components/Career.tsx
@@ -0,0 +1,111 @@
+import { JSX } from "react";
+
+export default function Career(): JSX.Element {
+  return (
+    <>
+      <section
+        className="sec sec2"
+        id="sec2"
+      >
+        <article className="inner">
+          <div className="wrap">
+            <ul>
+              <li>
+                <span className="img"></span>
+              </li>
+              <li>
+                <div className="sub_wrap">
+                  <span>
+                    성/Surname
+                    <br />
+                    <b>KIM</b>
+                  </span>
+                  <span>
+                    이름/Given names
+                    <br />
+                    <b>YEONJU</b>
+                  </span>
+                  <span>
+                    한글성명
+                    <br />
+                    <b>김연주</b>
+                  </span>
+                </div>
+                <span>
+                  자격증/Certificate
+                  <br />
+                  <b>2021 09.17 웹디자인기능사</b>
+                </span>
+
+                <div className="sub_wrap">
+                  <span>
+                    기술/Skill
+                    <br />
+                    <b>html5[상] - 하드코딩 가능</b>
+                    <br />
+                    <b>css3[상] - 하드코딩 및 TailWind Css 프레임워크 가능</b>
+                    <br />
+                    <b>jQuery[중]</b>
+                    <br />
+                    <b>
+                      JavaScript[상] - 바닐라 자바스크립트 및 오픈소스 활용가능
+                    </b>
+                    <br />
+                    <b>React[상] - 바닐라 자바스크립트 및 오픈소스 활용가능 </b>
+                    <br />
+                    <b>PHP[중] - 그누보드 활용가능</b>
+                    <br />
+                    <b>Adobe PhotoShop[상]</b>
+                    <br />
+                    <b>Adobe Illustrator[중]</b>
+                    <br />
+                    <b>Figma[상]</b>
+                  </span>
+                </div>
+                <div className="sub_wrap">
+                  <span>
+                    교육과정/Education
+                    <br />
+                    <b>
+                      2015 02 - 2017 02 <br /> 속초설악고등학교 졸업
+                    </b>
+                    <br />
+                    <b>
+                      2017 02 - 2019 02 <br /> 인천재능대학교 항공운항서비스과
+                      졸업
+                    </b>
+                    <br />
+                    <b>
+                      2021 05 - 2021 10 <br /> 하이미디어 UI/UX 웹디자인 &
+                      웹퍼블리셔
+                      <br className="Mo" />
+                      (프론트엔드) 과정 수료
+                    </b>
+                    <b>
+                      2024 11 - 2025 02 <br /> 바닐라코딩 프론트엔드 개발자
+                      부트캠프 코스 수료
+                    </b>
+                  </span>
+                </div>
+                <div className="sub_wrap">
+                  <span>
+                    경력/Career
+                    <br />
+                    <b>2018 12 - 2019 02 마이리얼트립 항공팀 인턴 근무</b>
+                    <br />
+                    <b>2019 06 - 2020 02 대한항공 VIP 라운지 근무</b>
+                    <br />
+                    <b>2020 02 - 2021 03 위드피아노 상암점 정매니저 근무</b>
+                    <br />
+                    <b>2021 10 - 2022 10 (주)우리플랫폼 웹에이전시 근무 </b>
+                    <b>2022 10 - 2024 10 현대카드 온라인마케팅팀 근무 </b>
+                  </span>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </article>
+      </section>
+    </>
+  );
+}
